8b8f896db4c66b8c5e65f35683b1bbd4a2bf6b8c
ywzheng2 <ywzheng2@iflytek.com>
2021-03-28 17:44:14 +0800

    commit project first time

--- git-test-project/src/com/zyw/git/FirstClass.java
+++ git-test-project/src/com/zyw/git/FirstClass.java
@@ -4,4 +4,10 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public int test() {
+        int i = 0;
+        i += 10;
+        return i;
+    }
 }
